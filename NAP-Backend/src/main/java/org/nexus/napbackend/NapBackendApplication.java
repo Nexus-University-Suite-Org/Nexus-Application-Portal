9e0fb80bfb3bae8baf8f932dfd65db4cf7621443
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-08-23 22:44:09 +0300

    Start Initialise Nexus Application Portal

---
+++ NAP-Backend/src/main/java/org/nexus/napbackend/NapBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nexus.napbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NapBackendApplication.class, args);
+    }
+
+}
---
+++ NAP-Backend/src/test/java/org/nexus/napbackend/NapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexus.napbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
