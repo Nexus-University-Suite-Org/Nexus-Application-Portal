f3cc126df142ee8e75d80d93eb90913dc3d4e3b4
Alvin11David <alvin69david@gmail.com>
2026-08-28 14:37:51 +0300

    Update project files

--- NAP-Backend/run.bat
+++ NAP-Backend/run.bat
@@ -1,3 +1,3 @@
 @echo off
 set MAVEN_OPTS=-Xmx1024m
-"C:\Users\ALVIN\AppData\Local\Temp\kilo\maven\apache-maven-3.9.6\bin\mvn.cmd" clean spring-boot:run
+"C:\Users\ALVIN\tools\apache-maven-3.9.16\bin\mvn.cmd" clean spring-boot:run
